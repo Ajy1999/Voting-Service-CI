d53353bb2bb5f51f5a93f4beb00951c11f74d610
Aishwarya Jain Yelaneeru <aishwaryajainy26@outlook.com>
2025-04-07 09:36:40 +0000

    Added comment -Updated app.py

--- vote/app.py
+++ vote/app.py
@@ -6,6 +6,8 @@ import random
 import json
 import logging
 
+
+#added new comment
 option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
